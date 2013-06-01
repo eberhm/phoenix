ac006e6d21148e4a46047b4b1a44b51d74d143b7
Eber Herrera <eberhm@gmail.com>
2013-06-01 20:41:06 +0200

    Now cdn decorator has a setBaseUrl method

--- src/Phoenix/Asset/UrlDecorator/Cdn.php
+++ src/Phoenix/Asset/UrlDecorator/Cdn.php
@@ -3,8 +3,18 @@ namespace Phoenix\Asset\UrlDecorator;
 
 class Cdn extends AbstractDecorator
 {
+    protected $baseUrl = 'http://cdn.mycdn.com';
+
     public function decorate($url)
     {
-        return 'http://cdn.mycdn.com'.parent::decorate($url);
+        return $this->baseUrl.parent::decorate($url);
+    }
+
+    /**
+     * @param string $baseUrl
+     */
+    public function setBaseUrl($baseUrl)
+    {
+        $this->baseUrl = $baseUrl;
     }
 }
